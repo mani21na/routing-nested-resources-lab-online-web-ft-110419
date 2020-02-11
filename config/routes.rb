972ca412992c5819803053de6a2d105e3c90fee1
Hanna Lee <mani21na@gmail.com>
2020-02-11 03:02:18 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :artists
-  resources :songs
-
   resources :artists do
     resources :songs, only: [:index, :show]
   end
+  
+  resources :artists
+  resources :songs
 end
